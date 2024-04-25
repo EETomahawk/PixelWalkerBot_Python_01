06093489c4cd6b440facb8bac000a0eb6d5728f6
Tomahawk <chris506@hotmail.co.uk>
2024-04-25 17:02:50 +0100

    Update plan.py

--- stuff/blockIDs/plan.py
+++ stuff/blockIDs/plan.py
@@ -6,7 +6,15 @@
 # Parse the subsequent list after the tilemap name in the JS to get the block names and locations in tilemap.
 # Use this info to generate separate PNGs for each block, along with their "filename"
 # Use this "filename" to find the more colloquial block names and their blockIDs elsewhere in the JS - see below
+# Build a markdown-formatted table of blocks
+# Consider doing the same with smileys
+# Find a way to check the game version programatically - parse client JS probably
+  # No need to update block list if version hasn't changed.... hopefully
 
+# Turn the above steps into a Python script
+# Look into using Python stdlib for the HTTP GETs to avoid dependency on external requests lib (optional)
+# Set up GitHub Action to run script and commit resulting .md table into this repo
+  # https://www.python-engineer.com/posts/run-python-github-actions/
 
 JS:
 n[n.GravityLeft = 1] = "GravityLeft",
