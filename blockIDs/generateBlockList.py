be500d3f7fe6a5fc583e2cb4cf1c979d36f2ce5c
Tomahawk <eeycc7@nottingham.ac.uk>
2024-07-21 00:35:17 +0100

    Replaced v1 block list script with v2.

--- blockIDs/generateBlockList.py
+++ blockIDs/generateBlockList.py
@@ -2,17 +2,19 @@
 
 #Summary:
 # Download https://pixelwalker.net/game.html.
-# Parse HTML to find game JS link - e.g. https://pixelwalker.net/assets/game-cwptkMuL.js.
-# Download game JS.
-# Check if game version has changed. If not, terminate.
-# Regex search JS for /assets/tile_atlas-######.png to get name of tilemap of all blocks.
+# Parse HTML to find game JS file URL and download the JS.
+# Search for game version in JS. If no update since last script execution, terminate script.
+# Regex search JS for /assets/tile_atlas-######.png to get name of tilemap containing all blocks.
 # Download tilemap png.
-# Parse the subsequent list after the tilemap name in the JS to get the block names and locations in tilemap.
-# Use this info to pull out the PNG of each block from the tilemap, along with its "filename".
-# Download https://game.pixelwalker.net/mappings JSON.
-# Do some voodoo to match up the block names with the tiles' "filenames".
-# Build a README markdown file with a table of blocks.
-# Use GitHub Actions to run this script periodically.
+# Parse the JS array of tilemap blocks to get the block names and locations in tilemap.
+# Use this to extract the PNG of each block from the tilemap, along with its "filename".
+# Get the dictionary in the initBlocks() function which lists blocks in block ID order.
+# All(?) real blocks have forward slashes, else they are block pack names.
+# The block names exactly match the tilemap block names.
+# Save all the block PNGs to ./images
+# Build a README table with the embedded block images, names and IDs.
+
+#TODO: decode JS initSmileys() to get smileys as well.
 
 import logging
 from traceback import format_exception
@@ -62,20 +64,19 @@ if currentGameVersion == lastVersionGenerated:
     print("Game version unchanged. Terminating.")
     exit(0)
 else: #Delete all PNGs in ./images.
-    for f in glob("./images/*.png"): remove(f)
+    for f in glob("./images/*.png"): remove(f) #TODO don't delete existing images if code below crashes.
 
 #PNG containing all blocks has similar naming format to JS file. Find it in the JS.
 #Find substring between "/assets/tile_atlas-" and '.png"'
 tilesetID = re.search('(?<="/assets/tile_atlas-)[^.]+(?=\.png")', gameJS).group()
 tilesetURL = f"{gameURL}/assets/tile_atlas-{tilesetID}.png" #Build URL to PNG containing all blocks.
-tilesetPNG = Image.open(BytesIO(requests.get(tilesetURL).content)) #Download PNG and load it into Pillow.
+tilesetPNG = Image.open(BytesIO(requests.get(tilesetURL).content)) #Download and open PNG.
 
-#Find substring after tileset filename between square brackets.
-#This is JS array of objects describing the blocks in the tileset and their names.
-tilesetJSArray = re.search("(?<=\[)[^\]]+(?=])", gameJS.split(tilesetID, 1)[1]).group()
-#Cannot easily convert this JS array of objects to JSON for parsing. Instead, manually parse it.
+#Find JS array [] containing filename:"background/basic/black"
+#This lists all the blocks in the tileset PNG and their names.
+tilesetJSArray = gameJS[gameJS.split('filename:"background/basic/black"')[0].rfind("["):].split("]",1)[0]
 
-tileNames = []
+tiles = {} #name: Image
 
 #Split JS array of tileset blocks apart using the block name and iterate it.
 for rawTile in tilesetJSArray.split("filename:")[1:]:
@@ -86,57 +87,30 @@ for rawTile in tilesetJSArray.split("filename:")[1:]:
     #Ignore w and h. Portals, coins, water, fire etc. are composed of multiple 16x16 tiles because they're animated,
     #so just use the first 16x16 tile of that section for the block image.
     x, y, _, _ = [int(v.split(":")[1]) for v in frame.split(",")]
-    blockImage = tilesetPNG.crop((x, y, x+16, y+16))
+    tiles[name] = tilesetPNG.crop((x, y, x+16, y+16))
 
-    tileName = name.replace('/','_')
 
-    #Manipulate the tileName a bit to improve matching.
-    #Remove prefixes that don't appear in mappings JSON.
-    for s in ["foreground_", "decoration_", "hazards_", "keys_",
-              "coin_doors_and_gates_", "extra_", "coins_", "generic_",
-              "death_", "spawn_", "liquids_", "switches_"]:
-        if tileName.startswith(s): tileName = tileName.removeprefix(s)
+#Retrieve the JS object of block names from the initBlocks() function.
+#Find substring between { and } AFTER initBlocks(){
+blockObject = re.search("(?<={)[^}]+", gameJS.split("initBlocks(){")[1]).group()
+#Get a list of the strings in the object. These are the package and block names.
+blockList = re.findall('"(.*?)"', blockObject)
 
-    #Move "background_" to end of string and change to "_bg".
-    if tileName.startswith("background_"): tileName = tileName[11:] + "_bg"
-    #Rearrange local/global switch strings to improve matching.
-    # elif tileName.startswith("switches_local"):
-    #     tileName = tileName.replace("switches_local", "local_switch")
-    # elif tileName.startswith("switches_global"):
-    #     tileName = tileName.replace("switches_global", "global_switch")
+blocks = [] #List with either package name (str) or Tuple(blockID, blockName, fileName)
+bID = 0
+for item in blockList:
+    if "/" not in item: #String is name of block pack.
+        continue
+        #blocks.append(item)
+    else: #String is block name.
+        #Tidy up block name.
+        #blockName = item.replace("extra/","").replace("foreground/","").replace("background/","")
+        blockName = item.replace("/", "_")
+        fileName = f"{bID}_{blockName}.png"
+        blocks.append((bID, blockName, fileName)) #Append block info tuple to list.
+        tiles[item].save(f"./images/{fileName}") #Save block as PNG to ./images/ dir.
+        bID += 1
 
-    blockImage.save(f"./images/{tileName}.png")
-    tileNames.append(tileName)
-
-mappingsURL = "https://game.pixelwalker.net/mappings"
-mappings = requests.get(mappingsURL).json() #Download mappings JSON. Convert to dict.
-
-blockList = {}
-#Do multiple passes at decreasing confidence levels to match up the strings.
-#Eliminate each matched pair to improve subsequent matches/iterations.
-cutoff = [1, 0.9, 0.8, 0.7, 0.5, 0]
-for n in cutoff:
-    for blockName, blockID in mappings.copy().items():
-        matches = get_close_matches(blockName, tileNames, n=1, cutoff=n)
-        if matches: #Non-empty list - i.e. match found at confidence above this cutoff.
-            tileName = matches[0]
-            blockList[blockID] = {
-                "blockName": blockName,
-                "imageName": tileName }
-            #Remove match from both collections, since we're doing multiple passes.
-            tileNames.remove(tileName)
-            del mappings[blockName]
-
-    #For debugging:
-#=============================================================================================
-#    if len(cutoff) > 1 and n == cutoff[-2]: blockList.clear()
-# print(f"{len(blockList)}/{len(mappings)+len(blockList)} matched. {len(mappings)} remaining.")
-#
-# with open("./mappings.txt", "w", encoding="utf-8") as file:
-#     file.writelines("\n".join(mappings))
-# with open("./tileset.txt", "w", encoding="utf-8") as file:
-#     file.writelines("\n".join(tileNames))
-#=============================================================================================
 
 timestamp = str(datetime.utcnow())[:-7] + " UTC"
 #Start of markdown file.
@@ -145,14 +119,18 @@ s += "## Block IDs:\n"
 s += "**WARNING:** This list is automatically generated and may have errors. "
 s += "Double-check whether the game version above matches the client version "
 s += "on pixelwalker.net\n\n"
+#s += "|Package|Image|ID|Name|\n|---|---|---|\n" #Table header.
 s += "|Image|ID|Name|\n|---|---|---|\n" #Table header.
 
-with(open("./README.md", "w", encoding="utf-8") as file): #Overwrite markdown file.
+with(open("./README.md", "w", encoding="utf-8") as file): #Overwrite existimng README file.
     file.write(s)
-    for ID, info in sorted(blockList.items()): #Sort by blockID.
-        n = info["imageName"] + ".png" #Filename for image alt text.
-        p = "./images/" + n #Relative path to image.
-        file.write(f"|![{n}]({p})|{ID}|{info['blockName']}|\n")
+    for item in blocks:
+        # if type(item) is str:
+        #     pass
+        # else:
+        blockID, blockName, fileName = item
+        path = "./images/" + fileName #Relative path to image.
+        file.write(f"|![{fileName}]({path})|{blockID}|{blockName}|\n")
 
 with open("./lastVersionGenerated.txt", "w", encoding="utf-8") as file: #Keep record of latest game version.
     file.write(currentGameVersion)
